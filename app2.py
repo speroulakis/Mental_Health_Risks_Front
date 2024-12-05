9f042243848cfe1b8710bbeeb7bcb7cb0053e056
sp <sebastian.peroulakis@hotmail.com>
2024-12-05 11:47:53 +0100

    oui

--- app2.py
+++ app2.py
@@ -1,25 +1,9 @@
 import streamlit as st
 import requests
 import matplotlib.pyplot as plt
-from scipy import misc
 
-st.markdown(
-    """
-    <style>
-    body {
-        background-color: #f0f8ff;
-    }
-    .stButton>button {
-        background-color: #4CAF50;
-        color: white;
-        border-radius: 8px;
-        font-size: 16px;
-        padding: 10px 20px;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
+import pandas as pd
+
 
 st.title("PharmaFeel")
 
@@ -45,20 +29,3 @@ if string :
 
 else :
     st.warning('Please enter some input')
-
-
-
-# Texte unique
-# if input_method == "Feeling":
-#     text_input = st.text_area("How do you feel :", "")
-#     if st.button("Analyze"):
-#         if text_input.strip():
-#             params2 = {
-#                 'st' : text_input}
-#             response = requests.get(url, params=params2)
-#             jean = response.json().get("prediction")
-#             #sentiment, confidence = call_api(text_input)
-#             st.write(f"**Feeling detected:** {jean}")
-#             #st.write(f"**Confiance :** {response:.2f}")
-#         else:
-#             st.warning("Please enter text.")
