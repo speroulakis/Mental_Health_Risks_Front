a44b85fce812def0cde852a48bf900a58633cf77
sp <sebastian.peroulakis@hotmail.com>
2024-12-05 10:41:01 +0100

    oui

--- app2.py
+++ app2.py
@@ -13,17 +13,17 @@ response = requests.get(url_lstm, params).json()
 pred = response.get('answer')
 st.write(f'YAAAAAYY the prediction is {pred}')
 
-# # Texte unique
-# if input_method == "Feeling":
-#     text_input = st.text_area("How do you feel :", "")
-#     if st.button("Analyze"):
-#         if text_input.strip():
-#             params2 = {
-#                 'st' : text_input}
-#             response = requests.get(url, params=params2)
-#             jean = response.json().get("prediction")
-#             #sentiment, confidence = call_api(text_input)
-#             st.write(f"**Feeling detected:** {jean}")
-#             #st.write(f"**Confiance :** {response:.2f}")
-#         else:
-#             st.warning("Please enter text.")
+# Texte unique
+if input_method == "Feeling":
+    text_input = st.text_area("How do you feel :", "")
+    if st.button("Analyze"):
+        if text_input.strip():
+            params2 = {
+                'st' : text_input}
+            response = requests.get(url, params=params2)
+            jean = response.json().get("prediction")
+            #sentiment, confidence = call_api(text_input)
+            st.write(f"**Feeling detected:** {jean}")
+            #st.write(f"**Confiance :** {response:.2f}")
+        else:
+            st.warning("Please enter text.")
